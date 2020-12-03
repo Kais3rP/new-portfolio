bdd3a1e5856a9c9b2769bb0549eccfd2a1997b38
Kais3rP <cesare.polonara@gmail.com>
2020-12-03 15:19:40 +0100

    finished about picture css animation

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,34 +3,52 @@ import { Row, Col } from "react-bootstrap";
 import TypeIt from "typeit-react";
 import "./index.css";
 import io from "../../pics/io.jpg"
+import { gsap, TimelineMax } from "gsap";
 
 
 export default function About() {
 
-  useEffect(()=>{
-
-  },[])
+  useEffect(() => {
+    let maskWidth = 0;
+    const moveFilter = new TimelineMax()
+      .to(maskRef.current, 0.2, { width: () => maskWidth })
+      .pause()
+    picContainerRef.current.addEventListener("mousemove", handleMouseMove);
+    function handleMouseMove(e) {
+      console.log(Math.floor(e.clientX - e.currentTarget.getBoundingClientRect().x) + "px" )
+       maskWidth = Math.floor(e.clientX - e.currentTarget.getBoundingClientRect().x) + "px";
+      if (moveFilter.isActive()) moveFilter.invalidate()
+      moveFilter.restart()
+    }
+  }
+    , [])
+  const picContainerRef = useRef();
+  const maskRef = useRef();
 
   return (
-    <Row >
+    <Row className="w-100">
       <Col className="w-100 d-flex flex-column justify-content-around align-items-center">
-      <div className="about-text"> 
-       <TypeIt  options={{
-          waitUntilVisible: true,
-          speed:10
-        }}>
-          <div className="d-flex ml-4 mt-2">
-            <p style={{ color: "#66ccff" }}>this.about()</p>
-          </div>
-          <div className="w-100 d-flex ml-4 mt-2">
-            <p style={{ color: "#ff6600" }}>
-              "My name is Cesare and I'm a Web Developer"
+        <div className="about-text">
+          <TypeIt options={{
+            waitUntilVisible: true,
+            speed: 10
+          }}>
+            <div className="d-flex ml-4 mt-2">
+              <p style={{ color: "#66ccff" }}>this.about()</p>
+            </div>
+            <div className="w-100 d-flex ml-4 mt-2">
+              <p style={{ color: "#ff6600" }}>
+                "My name is Cesare and I'm a Web Developer"
          </p>
-          </div>
-        </TypeIt></div>
-      
-        <img id="my-pic" src={io} alt="My photo"></img>
-        <img id="my-pic-mask" src={io} alt="My photo"></img>
+            </div>
+          </TypeIt>
+        </div>
+        <div ref={picContainerRef} id="pic-container" >
+          <img id="my-pic" src={io} alt="My photo"></img>
+          <div ref={maskRef} id="filter"></div>
+         
+        </div>
+
       </Col>
     </Row>
 
--- src/containers/mainwindow/index.js
+++ src/containers/mainwindow/index.js
@@ -11,71 +11,71 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 const MainWindowsHoc = React.memo(function ({ myRef, children }) {
-   
- useEffect(()=>{
-    if(myRef) 
-       new TimelineMax({scrollTrigger: { trigger: myRef.current, toggleActions: 'play none none none' }})
-  .to(myRef.current,5,{ css : {opacity:1 }, ease:"Back.easeOut"})
+
+  useEffect(() => {
+    if (myRef)
+      new TimelineMax({ scrollTrigger: { trigger: myRef.current, toggleActions: 'play none none none' } })
+        .to(myRef.current, 5, { css: { opacity: 1 }, ease: "Back.easeOut" })
 
     const localRef = myRef.current;
     const {
-        app,
-        Container,
+      app,
+      Container,
     } = createNewPixiApp(myRef.current.clientWidth, myRef.current.clientHeight, localRef);
 
-const firstContainer = new Container();
-const rect = new PIXI.Graphics();
-rect.scale.set(1);
-rect.alpha = 0.3;
-rect.beginFill(0x222222);
-rect.lineStyle(5, 0x000000);
-rect.drawRect(0, 0, myRef.current.clientWidth, myRef.current.clientHeight+30);
+    const firstContainer = new Container();
+    const rect = new PIXI.Graphics();
+    rect.scale.set(1);
+    rect.alpha = 0.3;
+    rect.beginFill(0x222222);
+    rect.lineStyle(5, 0x000000);
+    rect.drawRect(0, 0, myRef.current.clientWidth, myRef.current.clientHeight + 30);
 
-const filter = new CRTFilter();
-firstContainer.addChild(rect)
-firstContainer.filters = [filter]
+    const filter = new CRTFilter();
+    firstContainer.addChild(rect)
+    firstContainer.filters = [filter]
 
-app.stage.addChild(firstContainer);
-filter.curvature=10;
-filter.lineWidth = 0.5;
-filter.noise=0.1;
-filter.curvature=0.7;
-filter.lineContrast=1.5;
-app.ticker.add(updateCRTFilter)
-   
-    function updateCRTFilter(delta){
-        filter.seed = Math.random();
-            filter.time += 5;
+    app.stage.addChild(firstContainer);
+    filter.curvature = 10;
+    filter.lineWidth = 0.5;
+    filter.noise = 0.1;
+    filter.curvature = 0.7;
+    filter.lineContrast = 1.5;
+    app.ticker.add(updateCRTFilter)
+
+    function updateCRTFilter(delta) {
+      filter.seed = Math.random();
+      filter.time += 5;
     }
 
-      function handlePointerEnter(e){
-        filter.noiseSize = 1.2;  
-      }
-      function handlePointerLeave(e){
-        filter.noiseSize = 1;      
-      }
-      
-      function handleResize(){
-        rect.width = localRef.clientWidth;
-        rect.height = localRef.scrollHeight;
-      }
-      window.addEventListener("resize", handleResize)
-      localRef.addEventListener("pointerenter", handlePointerEnter)
-      localRef.addEventListener("pointerleave", handlePointerLeave)
-      return () => {
-        localRef.removeEventListener("pointerenter", handlePointerEnter)
-        localRef.removeEventListener("pointerleave", handlePointerLeave)
+    function handlePointerEnter(e) {
+      filter.noiseSize = 1.2;
+    }
+    function handlePointerLeave(e) {
+      filter.noiseSize = 1;
     }
 
-}, [myRef])
+    function handleResize() {
+      rect.width = localRef.clientWidth;
+      rect.height = localRef.scrollHeight;
+    }
+    window.addEventListener("resize", handleResize)
+    localRef.addEventListener("pointerenter", handlePointerEnter)
+    localRef.addEventListener("pointerleave", handlePointerLeave)
+    return () => {
+      localRef.removeEventListener("pointerenter", handlePointerEnter)
+      localRef.removeEventListener("pointerleave", handlePointerLeave)
+    }
 
-    return (
-        <Row className="justify-content-center align-items-center">
-            <Col xs={12} lg={6} id={myRef?.current ? myRef.current.id : null} ref={myRef} className={`window d-flex justify-content-center align-items-start p-5`}>
-{children}
-            </Col>
-        </Row>
-    )
+  }, [myRef])
+
+  return (
+    <Row className="justify-content-center align-items-center">
+      <Col xs={12} lg={6} id={myRef?.current ? myRef.current.id : null} ref={myRef} className={`window d-flex justify-content-center align-items-start p-5`}>
+        {children}
+      </Col>
+    </Row>
+  )
 })
 
 export default MainWindowsHoc
