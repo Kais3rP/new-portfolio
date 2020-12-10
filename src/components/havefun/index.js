88e86980d5a0858f1b9e0e007780a532fedd34c4
Kais3rP <cesare.polonara@gmail.com>
2020-12-10 11:09:16 +0100

    added under construction animation and havefun updated

--- src/components/havefun/index.js
+++ src/components/havefun/index.js
@@ -3,26 +3,26 @@ import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom"
 import "./index.css";
 import TypeIt from "typeit-react";
+import underConstr from "../../pics/under-constr.svg"
 
 
 
 export default function HaveFun({ havefunRef }) {
 
     return (
-
         <Row className="w-100">
         <Col className="w-100 d-flex flex-column">
-          <TypeIt  options={{
-          }}>
+          <TypeIt>
             <div className="d-flex ml-4 mt-2">
               <p style={{ color: "#66ccff" }}>this.haveFun()</p>
             </div>
             <div className="w-100 d-flex ml-4 mt-2">
               <p style={{ color: "#ff6600" }}>
-              "Here are some projects I worked on, they are classified by categories, front end only and full stack projects"
+              "Section Under Construction"
            </p>
             </div>
           </TypeIt>
+           <img className="under-construction" src={underConstr} style={{width:"20%"}} alt="under construction"/>
         </Col>
       </Row>
     )
